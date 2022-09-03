21794f2f2dcbfab68580ec465a234548a8ef7734
ivasquez <ivasquez-2021224@kinal.edu.gt>
2022-09-03 11:18:30 -0600

    Se agrego la estructura de proyecto (Cafeteria-Antojitos)

---
+++ Cafeteria-Antojitos/src/java/config/Conexion.java
@@ -0,0 +1,23 @@
+
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conexion {
+    Connection conexion;
+    
+    public Connection Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/CafeteriaAntojitos?useSSL=false", "root","");
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Ha Ocurrido un erro al conectar con la base de datos ");
+        }
+        
+        
+        return conexion;
+    }
+}
